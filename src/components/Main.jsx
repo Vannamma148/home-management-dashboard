b2c1b7d8712331f2de9f24e43a33c4c3aa17f651
Vannamma N A <83291550+Vannamma148@users.noreply.github.com>
2025-11-29 12:24:05 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,35 +1,37 @@
 // src/components/layout/Main.jsx
 "use client";
+import { Outlet } from "react-router-dom";
 import { useState } from "react";
 import Sidebar from "../components/Sidebar";
-import Navabar from "../components/Navbar";
+import Navbar from "../components/Navbar";
 
-export default function Main({ children }) {
+export default function Main() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="flex bg-gray-50">
-      
+    <div className="flex bg-gray-50 min-h-screen">
+
       {/* Sidebar */}
-      <Sidebar 
-        isOpen={sidebarOpen} 
-        toggleSidebar={() => setSidebarOpen(!sidebarOpen)} 
+      <Sidebar
+        isOpen={sidebarOpen}
+        toggleSidebar={() => setSidebarOpen(!sidebarOpen)}
       />
 
-      {/* Right section */}
-      <div className="flex flex-col flex-1 min-h-screen lg:ml-64">
-        
-        <Navabar toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+      {/* Right side */}
+      <div className="flex flex-col flex-1 lg:ml-64">
 
+        <Navbar toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+
+        {/* Page content */}
         <main className="p-6 mt-20">
-          {children}
+          <Outlet />
         </main>
-        <footer className="text-center text-gray-500 text-sm py-4 ">
-  © {new Date().getFullYear()} AVHome — All rights reserved.
-</footer>
 
+        <footer className="text-center text-gray-500 text-sm py-4">
+          © {new Date().getFullYear()} AVHome — All rights reserved.
+        </footer>
       </div>
-      
+
     </div>
   );
 }
